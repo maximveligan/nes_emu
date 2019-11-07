650ea86d3c1293355bdf17863e2e603aa7c3ad39
maximveligan <maximveligan@gmail.com>
2019-11-06 19:02:37 -0800

    Added wasm support

--- nes-wasm/src/lib.rs
+++ nes-wasm/src/lib.rs
@@ -3,6 +3,8 @@ extern crate nes_emu;
 mod utils;
 
 use wasm_bindgen::prelude::*;
+use nes_emu::NesEmulator;
+use nes_emu::rom::load_rom;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -11,11 +13,32 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+pub struct BufferStruct {
+    pub pointer: *const u8,
+    pub length: usize,
 }
 
 #[wasm_bindgen]
-pub fn greet(msg: &str) {
-    alert(msg);
+pub struct EmuInterface {
+    // ctrl0
+    // ctrl1
+    nes_emu: NesEmulator,
+}
+
+#[wasm_bindgen]
+impl EmuInterface {
+    pub fn new(buffer: &[u8]) -> Result<EmuInterface, JsValue> {
+        let rom = match load_rom(buffer) {
+            Ok(r) => r,
+            Err(e) => return Err(JsValue::from_str(&e.to_string())),
+        };
+        Ok(EmuInterface {
+            nes_emu: NesEmulator::new(rom),
+        })
+    }
+
+    pub fn get_frame(&mut self) -> BufferStruct {
+        let buffer = self.nes_emu.next_frame();
+        BufferStruct { pointer: buffer.as_ptr(), length: buffer.len() }
+    }
 }
